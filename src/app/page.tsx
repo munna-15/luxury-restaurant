bb8f365cd1aba7a9cd1a816af102b3ed101538da
Mustafijur Munna <mustafijurrahman0610@gmail.com>
2026-09-21 14:30:14 +0600

    fix: finalize page titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+
 import Navbar from "@/components/layouts/Navbar";
 import Hero from "@/components/home/Hero";
 import WelcomeSection from "@/components/home/WelcomeSection";
@@ -7,21 +8,18 @@ import OfferSection from "@/components/home/OfferSection";
 import HappyCustomers from "@/components/home/HappyCustomers";
 import Footer from "@/components/layouts/Footer";
 
-export const metadata: Metadata = { title: "Home" };
+export const metadata: Metadata = {
+  title: "Home | Maison.",
+};
 
 export default function Home() {
   return (
     <main className="relative overflow-x-clip">
       <Navbar />
-
       <Hero />
-
       <WelcomeSection />
-
       <MenuPreview />
-
       <OfferSection />
-
       <HappyCustomers />
       <Footer />
     </main>
